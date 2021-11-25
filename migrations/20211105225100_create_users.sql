b0d4bf22ce83c76ffd0bfc3fc3291868fde4c328
Théophile Choutri <theophile@choutri.eu>
2021-11-25 12:05:08 +0100

    Make a lot more user fields not null

--- migrations/20211105225100_create_users.sql
+++ migrations/20211105225100_create_users.sql
@@ -1,12 +1,14 @@
 create table users (
   user_id uuid primary key,
-  username text unique,
-  display_name text,
-  email text unique,
-  password text,
-  imported bool,
-  created_at timestamptz,
-  updated_at timestamptz
+  username text unique not null,
+  display_name text not null,
+  email text unique not null,
+  password text not null,
+  imported bool not null,
+  created_at timestamptz not null,
+  updated_at timestamptz not null
 );
 
 create unique index on users (lower(username));
+create unique index on users (lower(email));
+create unique index on users (imported);
