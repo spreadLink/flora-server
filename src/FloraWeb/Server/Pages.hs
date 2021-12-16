6ca88ab176c62fa05a9e049fafb7ee7ed9213441
Théophile Choutri <theophile@choutri.eu>
2021-12-16 12:43:21 +0100

    Fix authentication

--- src/FloraWeb/Server/Pages.hs
+++ src/FloraWeb/Server/Pages.hs
@@ -1,5 +1,6 @@
 module FloraWeb.Server.Pages where
 
+import Control.Monad.Reader
 import Lucid
 import Servant
 import Servant.API.Generic
@@ -8,11 +9,15 @@ import Servant.Server.Generic
 
 import qualified Data.Map.Strict as Map
 import qualified FloraWeb.Server.Pages.Admin as Admin
+import Flora.Environment
+import FloraWeb.Server.Auth
 import qualified FloraWeb.Server.Pages.Packages as Packages
 import FloraWeb.Templates
+import FloraWeb.Templates.Error
 import qualified FloraWeb.Templates.Pages.Home as Home
 import FloraWeb.Templates.Types
-import FloraWeb.Types
+import Network.HTTP.Types
+import Optics.Core
 
 type Routes = ToServantApi Routes'
 
@@ -24,21 +29,30 @@ data Routes' mode = Routes'
   }
   deriving stock (Generic)
 
-server :: ToServant Routes' (AsServerT FloraM)
+server :: ToServant Routes' (AsServerT FloraPageM)
 server = genericServerT Routes'
   { home = homeHandler
   , about = aboutHandler
-  , admin = Admin.server
+  , admin = ensureUser adminHandler
   , packages = Packages.server
   }
 
-homeHandler :: FloraM (Html ())
+ensureUser :: FloraAdminM a -> FloraPageM a
+ensureUser adminM = do
+  mUser <- asks (\callInfo -> callInfo ^. #userInfo)
+  case mUser of
+    Nothing -> renderError forbidden403
+    Just user -> withReaderT (\CallInfo{floraEnv} -> AuthedUser {userInfo = user, floraEnv}) adminM
+
+homeHandler :: FloraPageM (Html ())
 homeHandler = do
   let assigns = mkAssigns emptyAssigns (Just (UserAssigns $ Map.fromList [("navbar-search", "false")]))
   render assigns Home.show
 
-aboutHandler :: FloraM (Html ())
+aboutHandler :: FloraPageM (Html ())
 aboutHandler = do
   let assigns = emptyAssigns
   render assigns Home.about
 
+adminHandler :: FloraAdminM (Html ())
+adminHandler = undefined
